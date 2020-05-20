3fc7de1605886ca571f3a24ead275171911a7335
Aditya Hajare <aditya43@gmail.com>
2020-05-20 21:13:39 +0530

    Added validateInput() | Validate user inputs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
@@ -40,8 +41,45 @@ func takeAction(w []string) {
 	restartProgram()
 }
 
+// Validate user inputs
 func validateInput(w []string) error {
-	// Validate user inputs
+	var pieceErr, posErr bool
+
+	if len(w) < 2 {
+		return errors.New("Invalid input. Please enter piece type and position.")
+	}
+
+	switch strings.ToLower(w[0]) {
+	case "king", "queen", "bishop", "horse", "rook", "pawn":
+	default:
+		pieceErr = true // User didn't input any of the above piece type
+	}
+
+	switch strings.ToLower(w[1]) {
+	case "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8",
+		"b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8",
+		"c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8",
+		"d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8",
+		"e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8",
+		"f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8",
+		"g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8",
+		"h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8":
+	default:
+		posErr = true // User didn't input any of the above cell positions
+	}
+
+	if pieceErr && posErr {
+		return errors.New("Invalid piece type and position value.")
+	}
+
+	if pieceErr {
+		return errors.New("Invalid piece type.")
+	}
+
+	if posErr {
+		return errors.New("Invalid position value.")
+	}
+
 	return nil
 }
 
