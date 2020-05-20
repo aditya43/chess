390b9695d8fab27ee5514040e8620986754309a0
Aditya Hajare <aditya43@gmail.com>
2020-05-20 19:20:58 +0530

    Basic skeleton with bufio.Reader to read for input text on terminal screen

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aditya43/chess
+
+go 1.14
+
+require github.com/gookit/color v1.2.5
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strings"
+	// For printing colorful text in terminal
+)
+
+var cr *bufio.Reader
+
+func init() {
+	// Create chessboard
+}
+
+// Execute program
+func main() {
+	for {
+		cr = bufio.NewReader(os.Stdin)
+		printInfo()
+		s, _ := cr.ReadString('\n')
+		w := strings.Fields(strings.ToLower(s))
+		checkExit(s)
+		takeAction(w)
+	}
+}
+
+func takeAction(w []string) {
+	err := validateInput(w)
+
+	if err != nil {
+		// Print error
+		restartProgram()
+		return
+	}
+	printOutput()
+	restartProgram()
+}
+
+func validateInput(w []string) error {
+	// Validate user inputs
+	return nil
+}
+
+func printInfo() {
+	// Print information about how to use this program
+}
+
+func checkExit(s string) {
+	// Exit program if user typed 'exit'
+}
+
+func restartProgram() {
+	// Ask user to hit Enter key to restart program
+	// User can quit program by typing 'exit'
+}
+
+func printOutput() {
+	// Print available move positions and a chaseboard
+}
