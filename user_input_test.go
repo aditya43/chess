ebf484d4ce406a67cd1376d5b463276388fb0b15
Aditya Hajare <aditya43@gmail.com>
2020-05-20 21:47:33 +0530

    User input tests | TestForInvalidPositionInput

--- user_input_test.go
+++ user_input_test.go
@@ -21,3 +21,12 @@ func TestForInvalidPieceNameInput(t *testing.T) {
 		t.Fatalf("Failed to validate invalid piece name user input! Error: %v", err)
 	}
 }
+
+// Test if error is returned when user inputs an invalid position
+func TestForInvalidPositionInput(t *testing.T) {
+	err := validateInput([]string{"rook", "x1"})
+
+	if err == nil {
+		t.Fatalf("Failed to validate invalid position user input! Error: %v", err)
+	}
+}
