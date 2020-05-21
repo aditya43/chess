bd4307e38ad9cab9fc85c8e2e92e090f06f8e7a7
Aditya Hajare <aditya43@gmail.com>
2020-05-21 07:19:12 +0530

    Added CreateChessBoard() | Create new chessboard with | Create cells with their corrosponding string and numeric identifiers

--- chessboard.go
+++ chessboard.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type ChessBoard struct {
 	cells    map[int]string // Numeric Position to Text Representation. For e.g. 10 -> B2
 	pieces   map[int]*Piece // Numeric Position --> Piece. For printing purposes only
@@ -9,7 +11,23 @@ type ChessBoard struct {
 // Create new chessboard
 // Create cells with their numeric identifiers
 func CreateChessBoard() *ChessBoard {
-	//
+	b := &ChessBoard{}
+	b.cells = make(map[int]string)
+	b.strCells = make(map[string]int)
+
+	l := [8]string{"a", "b", "c", "d", "e", "f", "g", "h"} // Columns
+	num := 1
+
+	for _, c := range l {
+		for i := 1; i < 9; i++ {
+			str := fmt.Sprintf("%v%v", c, i) // Generate string position e.g. a1, h8 etc..
+			b.cells[num] = str
+			b.strCells[str] = num
+			num++
+		}
+	}
+
+	return b
 }
 
 // Place piece on a chessboard and update
