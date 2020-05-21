b5e735c4b5001c20fd50d0e456506e010ed7adb7
Aditya Hajare <aditya43@gmail.com>
2020-05-21 09:18:45 +0530

    Added setBoundries() method | Boundries will be set to pieces which can move on X, Y axis as well as in diagonal directions

--- boundries.go
+++ boundries.go
@@ -19,3 +19,65 @@ type DiagonalBoundry struct {
 	bottomLeft  int
 	bottomRight int
 }
+
+// Calculate left boundry based off current position of a piece
+func (p *Piece) setLeftBoundry() {
+	//
+}
+
+// Calculate right boundry based off current position of a piece
+func (p *Piece) setRightBoundry() {
+	//
+}
+
+// Calculate top boundry based off current position of a piece
+func (p *Piece) setTopBoundry() {
+	//
+}
+
+// Calculate bottom boundry based off current position of a piece
+func (p *Piece) setBottomBoundry() {
+	//
+}
+
+// Calculate top-left boundry based off current position of a piece
+func (p *Piece) setTopLeftBoundry() {
+	//
+}
+
+// Calculate top-right boundry based off current position of a piece
+func (p *Piece) setTopRightBoundry() {
+	//
+}
+
+// Calculate bottom-right boundry based off current position of a piece
+func (p *Piece) setBottomRightBoundry() {
+	//
+}
+
+// Calculate bottom-left boundry based off current position of a piece
+func (p *Piece) setBottomLeftBoundry() {
+	//
+}
+
+// Set boundries for a piece
+func (p *Piece) setBoundries() {
+	if p.xAllow {
+		p.setLeftBoundry()
+		p.setRightBoundry()
+	}
+
+	if p.yAllow {
+		p.setTopBoundry()
+		p.setBottomBoundry()
+	}
+
+	if p.crossAllow {
+		p.setTopLeftBoundry()
+		p.setTopRightBoundry()
+		p.setBottomRightBoundry()
+		p.setBottomLeftBoundry()
+	}
+
+	// fmt.Printf("%v %8v: %v %v %v %v %v %v %v %v \n", p.symbol, p.name, p.xBoundry.left, p.diagonalBoundry.topLeft, p.yBoundry.top, p.diagonalBoundry.topRight, p.xBoundry.right, p.diagonalBoundry.bottomRight, p.yBoundry.bottom, p.diagonalBoundry.bottomLeft)
+}
