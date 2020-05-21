70396256bb652c0a0f9b0978af20d7c578629923
Aditya Hajare <aditya43@gmail.com>
2020-05-21 09:09:01 +0530

    Added print() method on chessboard | Render chessboard with a piece

--- chessboard.go
+++ chessboard.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/gookit/color"
+)
 
 type ChessBoard struct {
 	cells    map[int]string // Numeric Position to Text Representation. For e.g. 10 -> B2
@@ -44,5 +49,40 @@ func (b *ChessBoard) placePiece(pos int, p *Piece) {
 
 // Render chessboard with a piece
 func (b *ChessBoard) print(p *Piece) {
-	//
+	row := 8
+	cnt := 0
+	cell := 8
+	for i := 0; i < 64; i++ {
+		if cnt == 8 {
+			cnt = 0
+			row -= 1
+			cell = row
+			fmt.Println()
+		}
+
+		// Print piece symbol if we are at current position of a piece
+		if p.curPos == cell {
+			s := color.New(color.FgWhite, color.BgRed, color.OpBold).Sprintf(" %v ", p.symbol)
+			// fmt.Printf("%6v %v ", b.cells[cell], s)
+			color.Red.Printf("%6v", strings.ToUpper(b.cells[cell]))
+			fmt.Printf(" %v ", s)
+			// color.BgRed.Printf("%6v  %v  ", b.cells[cell], p.symbol)
+		}
+
+		// If a piece can move to this cell, print it in green
+		if _, ok := p.availPos[cell]; ok {
+			// color.Green.Printf("%6v (%2v)", strings.ToUpper(b.cells[cell]), cell)
+			color.Green.Printf("%6v  %2v ", strings.ToUpper(b.cells[cell]), "")
+		}
+
+		// Piece is not at this cell and neither can move to this location
+		if _, ok := p.availPos[cell]; !ok && p.curPos != cell {
+			// fmt.Printf("%6v (%2v)", strings.ToUpper(b.cells[cell]), cell)
+			fmt.Printf("%6v  %2v ", strings.ToUpper(b.cells[cell]), "")
+		}
+
+		cell += 8
+		cnt++
+	}
+	fmt.Println()
 }
