1b4e9de268d0ad9d8502234cfd890b41626f4ef1
Aditya Hajare <aditya43@gmail.com>
2020-05-21 08:05:08 +0530

    Chessboard Tests | Test if strCells in chessboard represents correct numeric position on chessboard

--- chessboard_test.go
+++ chessboard_test.go
@@ -10,3 +10,20 @@ func TestChessBoardHas64Cells(t *testing.T) {
 		t.Fatalf("Failed to test if a chessboard has 64 cells in it, Number of cells: %v", len(cb.cells))
 	}
 }
+
+// Test if strCells in chessboard represents correct numeric position on chessboard
+func TestStrCellsInChessBoardRepresentsCorrectNumericPositionOnChessBoard(t *testing.T) {
+	inputs := map[string]int{
+		"a1": 1,
+		"d5": 29,
+		"e2": 34,
+		"b8": 16,
+		"c7": 23,
+	}
+
+	for i, v := range inputs {
+		if cb.strCells[i] != v {
+			t.Fatalf("Failed to Test if strCells in chessboard represents correct numeric position on chessboard. For cell %v, it represents %v numeric position. Expected output: %v ---> %v", cb.strCells[i], v, i, v)
+		}
+	}
+}
