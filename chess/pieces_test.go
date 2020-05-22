96a8d8fc86814b219cc4a8835a1007f51f5e530b
Aditya Hajare <aditya43@gmail.com>
2020-05-22 09:05:57 +0530

    Test if a CreatePiece() func creates a piece

--- chess/pieces_test.go
+++ chess/pieces_test.go
@@ -14,3 +14,12 @@ func TestCreatePieceReturnsPointerToPieceType(t *testing.T) {
 		t.Error("Failed to test if a CreatePiece() func returns pointer to the Piece type!")
 	}
 }
+
+// Test if a CreatePiece() func creates a piece
+func TestCreatePieceCreatesPiece(t *testing.T) {
+	p := CreatePiece(20, "pawn") // Create piece
+
+	if p.Kind != "pawn" {
+		t.Error("Failed to test if a CreatePiece() func creates a piece")
+	}
+}
