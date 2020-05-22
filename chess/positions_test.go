6cf0c33aa072687c57a325b93bffc0d0ede49107
Aditya Hajare <aditya43@gmail.com>
2020-05-22 09:01:51 +0530

    Test move positions are generated in top direction for a non pawn type piece

--- chess/positions_test.go
+++ chess/positions_test.go
@@ -17,3 +17,17 @@ func TestCorrectMovePositionIsGeneratedForPawnTypePiece(t *testing.T) {
 
 	p = nil // Delete piece
 }
+
+// Test move positions are generated in top direction for a non pawn type piece
+func TestMovePositionsAreGeneratedInTopDirectionForNonPawnTypePiece(t *testing.T) {
+	cb := CreateChessBoard()
+	p := CreatePiece(20, "rook") // Create piece
+	p.AvailPos = make(map[int]bool)
+	p.addPositions(cb, p.yBoundry.top, p.maxY, Positive, 1)
+
+	if !p.AvailPos[21] && !p.AvailPos[22] && !p.AvailPos[23] && !p.AvailPos[24] {
+		t.Errorf("Failed to test if positions are generated in top direction for a non pawn type piece! Got: %v", p.AvailPos)
+	}
+
+	p = nil // Delete piece
+}
