202af93911dcc0df0a85b96c6060f8c0cce28e0a
Aditya Hajare <aditya43@gmail.com>
2020-05-22 09:01:29 +0530

    Test if correct move position is generated for pawn type piece

--- chess/positions_test.go
+++ chess/positions_test.go
@@ -1,1 +1,19 @@
 package chess
+
+import (
+	"testing"
+)
+
+// Test if correct move position is generated for pawn type piece
+func TestCorrectMovePositionIsGeneratedForPawnTypePiece(t *testing.T) {
+	cb := CreateChessBoard()
+	p := CreatePiece(20, "pawn") // Create piece
+	p.AvailPos = make(map[int]bool)
+	p.addPositions(cb, p.yBoundry.top, p.maxY, Positive, 1)
+
+	if !p.AvailPos[21] {
+		t.Error("Failed to test if correct move position is generated for pawn type piece!")
+	}
+
+	p = nil // Delete piece
+}
