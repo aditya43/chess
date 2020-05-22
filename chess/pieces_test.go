3f02fac3843df97ae53ee52744c91dbf20bb7b5e
Aditya Hajare <aditya43@gmail.com>
2020-05-22 09:05:37 +0530

    Test if a CreatePiece() func returns pointer to the Piece type

--- chess/pieces_test.go
+++ chess/pieces_test.go
@@ -1,1 +1,16 @@
 package chess
+
+import (
+	"reflect"
+	"testing"
+)
+
+// Test if a CreatePiece() func returns pointer to the Piece type
+func TestCreatePieceReturnsPointerToPieceType(t *testing.T) {
+	p := CreatePiece(20, "pawn") // Create piece
+	str := reflect.TypeOf(p).String()
+
+	if str != "*chess.Piece" {
+		t.Error("Failed to test if a CreatePiece() func returns pointer to the Piece type!")
+	}
+}
